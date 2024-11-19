5505d0e850e3f91845bfd8a959f728d548e32331
fei <365877516@qq.com>
2024-11-19 23:27:09 +0800

    first commit

--- src/main/java/om/foottrr/EurekaServerApplication.java
+++ src/main/java/om/foottrr/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package org.example;
+package om.foottrr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,14 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+
+    /**
+     * Chinese translation english
+     * @param args
+     */
     public static void main(String[] args) {
+        //Chinese translation eeee
         SpringApplication.run(EurekaServerApplication.class, args);
+        System.out.println("Chinese test english");
     }
 }
